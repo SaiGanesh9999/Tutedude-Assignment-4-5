2d8b916203badeaa2d89ae822a7d2742be9a9bb7
DEVARASETTI SAI GANESH <110873642+SaiGanesh9999@users.noreply.github.com>
2025-08-17 11:47:49 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+def read_file():
+    try:
+        with open("sample.txt", "r") as file:
+            print("File content:\n")
+            for line in file:
+                print(line.strip())
+    except FileNotFoundError:
+        print("Error: The file 'sample.txt' does not exist.")
+
+if __name__ == "__main__":
+    read_file()
